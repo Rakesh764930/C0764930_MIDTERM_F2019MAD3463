b4d72cde2f0c65352cd40ff790bf8d42a0d8e5e8
macstudent <rakeshkumar92215@gmail.com>
2019-09-25 19:20:16 -0400

    reverse created

---
+++ src/main/java/com/lambton/LambtonStringTools.java
@@ -0,0 +1,21 @@
+package com.lambton;
+
+public class LambtonStringTools {
+
+    public static String reverse(String  s) {
+        String nString=s;
+        String rString = " ";
+        for (int i = nString.length() - 1; i >= 0; i--) {
+            rString = rString + nString.charAt(i);
+        }
+
+
+        return(rString);
+    }
+
+public static int binaryToDecimal()
+
+
+
+
+}
---
+++ src/main/java/com/lambton/Test.java
@@ -0,0 +1,14 @@
+package com.lambton;
+
+public class Test {
+
+
+    public static void main(String[] args) {
+
+        LambtonStringTools rev=new LambtonStringTools();
+        String res=rev.reverse("Lambton");
+        System.out.println("the reverse string is "+res);
+
+
+    }
+}
